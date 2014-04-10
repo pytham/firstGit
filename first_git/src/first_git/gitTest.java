f22a15b496d456844ea720a7862778cdbc71da8e
pytham <gouthamraju.k@gmail.com>
2014-04-10 21:59:41 +0530

    second commit

--- first_git/src/first_git/gitTest.java
+++ first_git/src/first_git/gitTest.java
@@ -4,7 +4,7 @@ public class gitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Git is fun");
+		 System.out.println("Git is fun..is it?");
 	}
 
 }
